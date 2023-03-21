41430dedf10feff1ed54a76beba052062847aec9
piecoder-79 <pcnox7@gmail.com>
2023-03-21 17:34:24 +0530

    latest

--- index.js
+++ index.js
@@ -8,10 +8,28 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.render("index", { video: "" });
+  res.render("index");
 });
 
-app.post("/show_availables", async (req, res) => {
+app.post("/filter", async (req, res) => {
+  if (req.body.url == "") {
+    res.send("You didn't entered anything...");
+  } else {
+    const thumbnail = await ytdl.getInfo(req.body.url).then((info) => {
+      const length_of_thumbnail = info.videoDetails.thumbnail.thumbnails.length;
+      const fthumbnail =
+        info.videoDetails.thumbnail.thumbnails[length_of_thumbnail - 1].url;
+      const titleofvideo = info.videoDetails.title;
+      res.render("download", {
+        thumbnail: fthumbnail,
+        title: titleofvideo,
+        url: req.body.url,
+      });
+    });
+  }
+});
+
+app.post("/download", async (req, res) => {
   if (req.body.url == "") {
     res.send("You didn't entered anything...");
   } else {
