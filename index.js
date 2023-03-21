95dcfe83c4a6b0019df2a6b4e7a202c285a1f4f6
piecoder-79 <pcnox7@gmail.com>
2023-03-21 11:21:51 +0530

    all commits

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const path = require("path");
 const app = require("express")();
 const ytdl = require("ytdl-core");
 const bodyParser = require("body-parser");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
   res.render("index", { video: "" });
 });
+
 app.post("/show_availables", async (req, res) => {
   const fileName = await ytdl.getInfo(req.body.url).then((filename) => {
     return filename.videoDetails.title;
@@ -13,7 +18,9 @@ app.post("/show_availables", async (req, res) => {
   res.setHeader("Content-Type", "video/mp4");
   res.setHeader(
     "Content-Disposition",
-    `attachment; filename="jvxle.vercel.app - ${encodeURIComponent(fileName)}.mp4"`
+    `attachment; filename="jvxle.vercel.app - ${encodeURIComponent(
+      fileName
+    )}.mp4"`
   );
   const stream = ytdl(req.body.url, {
     filter: (format) => {
